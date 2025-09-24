d7fc69f38ae3695e37af57615d318bc045b988b9
Rafael Aguiar <rafael.aguiar02@gmail.com>
2025-09-24 13:00:01 -0300

    fix to work with different project as front end

--- Program.cs
+++ Program.cs
@@ -3,10 +3,21 @@ using SignalRWebpack.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseRouting();
+app.UseCors();
 
 app.MapHub<ChatHub>("/hub");
 
