c2dce890fd7f40f984f8d10137e27e3b4cca8d78
Rafael Aguiar <rafael.aguiar02@gmail.com>
2025-09-24 15:52:49 -0300

    small fix

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,6 +4,6 @@ namespace SignalRWebpack.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task NewMessage(string username, string message) =>
-        await Clients.All.SendAsync("messageReceived", username, message);
+    public async Task NewMessage(string username, string message, string type) =>
+        await Clients.All.SendAsync("messageReceived", username, message, "user");
 }
